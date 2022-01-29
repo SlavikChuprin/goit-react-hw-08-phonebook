7731c0dedc7cb5c6cf9cb883b23ceeea67f5a6b4
SlavikChuprin <chuprin.00@yandex.ru>
2022-01-29 22:36:28 +0300

    add saving user after refresh page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import Contacts from './components/Contacts';
 import LoginView from './views/LoginView';
 import RegisterView from './views/RegisterView';
 import AppBar from './components/AppBar';
+import { authOperations } from './redux/auth';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <AppBar />
